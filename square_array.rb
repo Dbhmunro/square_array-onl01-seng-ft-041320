327091a4b2978cd79d2a4e3261c66bf6861ffb96
David Munro <dbhmunro@gmail.com>
2020-04-16 02:59:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,5 @@
 def square_array(array)
   new_array = []
-  array.collect { |x| new_array << x ** 2 }
+  array.each { |x| new_array << x ** 2 }
   new_array
 end
-
-sample = [1, 2, 3]
-
-puts square_array(sample)
